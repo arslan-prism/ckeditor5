2689296a93f560f5d2fdcd100bfd0d6a9b8a2db4
arslan-prism <arslan.prism@gmail.com>
2021-02-03 22:47:08 +0500

    added codeblock

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -29,8 +29,10 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
 
-export default class ClassicEditor extends ClassicEditorBase {}
+export default class ClassicEditor extends ClassicEditorBase { }
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
@@ -56,7 +58,9 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	Code,
+	CodeBlock
 ];
 
 // Editor configuration.
@@ -74,6 +78,9 @@ ClassicEditor.defaultConfig = {
 			'indent',
 			'outdent',
 			'|',
+			'code',
+			'codeBlock',
+			'pre',
 			'imageUpload',
 			'blockQuote',
 			'insertTable',
@@ -88,6 +95,14 @@ ClassicEditor.defaultConfig = {
 			'imageStyle:side',
 			'|',
 			'imageTextAlternative'
+		]
+	},
+	codeBlock: {
+		languages: [
+			{ language: 'css', label: 'CSS' },
+			{ language: 'html', label: 'HTML' },
+			{ language: 'php', label: 'PHP' },
+			{ language: 'javascript', label: 'JavaSript' },
 		]
 	},
 	table: {
